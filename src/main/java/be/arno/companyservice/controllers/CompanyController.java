bac1032a01015fc731605505e85647a6baf63cc4
Arno Bruynseels <arno.bruynseels@telenet.be>
2018-06-25 15:06:36 +0200

    Working Java backend

--- src/main/java/be/arno/companyservice/controllers/CompanyController.java
+++ src/main/java/be/arno/companyservice/controllers/CompanyController.java
@@ -1,12 +1,11 @@
 package be.arno.companyservice.controllers;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
-import be.arno.companyservice.repositories.CarClient;
+import be.arno.companyservice.models.Company;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/companies", produces = APPLICATION_JSON_UTF8_VALUE)
 public class CompanyController {
 
-  @Autowired
-  private CarClient carClient;
-
-  @GetMapping(value = "/hello")
-  public String helloFromCompanyService() {
-    return "Hello from Company-Service";
-  }
-
-  @GetMapping(value = "/cars/{companyId}")
-  public List<String> getAllCarsForCompany(@PathVariable Long companyId) {
-    return carClient.getAllCarsForCompany(companyId);
+  @GetMapping(value = "/all")
+  public List<Company> getAllcompanies() {
+    return newArrayList(
+        new Company(1, "Belfius"),
+        new Company(2, "KBC"),
+        new Company(3, "ING"),
+        new Company(4, "Argenta")
+    );
   }
 
 }
